5372ab5b22d4e13b0c36a54a0795656622c65094
Tanvi Rastogi <tanvi.rastogi892@gmail.com>
2019-03-28 22:18:28 +0530

    Created a separate page for plans and loaded into the index.js

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ require('./vendors/css/grid.css');
 require('./resources/css/style.css');
 require('./resources/css/queries.css');
 require('./resources/js/script');
+
+document.getElementById('/section-meals');
